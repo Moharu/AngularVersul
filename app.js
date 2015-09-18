f030c103cfecab3e8a6402611589efce6416dbdd
Moharu <joaotelo.nh@hotmail.com>
2015-09-18 12:25:36 +0000

    f7 change

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.controller('mainController', function($scope, Pay){
         Pay.findOrders().then(
             function(success){
                 console.log(success);
-                //$scope.orders = success.data.hits.hits;
             },
             function(error){
                 console.log("error");
